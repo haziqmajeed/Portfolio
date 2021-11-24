7bde828cbef3f677b172d02550f7f4104ba33845
haziqmajeed <haziq.majeed57@gmail.com>
2021-11-24 16:10:50 +0500

    Nest JS and Angular

---
+++ Work/Angular_Nest/Nest Js/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import {MongooseModule} from '@nestjs/mongoose'
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [ProductsModule, MongooseModule.forRoot('mongodb+srv://dbUser:Pd9AI8EF1a5iMZyF@cluster0.fh8ms.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
